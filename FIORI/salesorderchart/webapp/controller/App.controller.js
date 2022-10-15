ad539d454b71b4f2792e6889504f51c55d74c696
uju <grruju@gmail.com>
2022-10-15 19:33:05 +0900

    221015

---
+++ FIORI/salesorderchart/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("sync.c203.salesorderchart.controller.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ FIORI/salesorderchart/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/c203/salesorderchart/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
