cbd936f5491e55ab0ca9540f2749af1f7803c0c6
uju <grruju@gmail.com>
2022-10-16 18:53:58 +0900

    22

---
+++ FIORI/productdetail/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("sync.c203.productdetail.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
