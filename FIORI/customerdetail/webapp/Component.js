6f48aba6604559257764ea7af8668db795f7f415
uju <grruju@gmail.com>
2022-10-16 23:11:35 +0900

    12

---
+++ FIORI/customerdetail/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("sync.c203.customerdetail.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
